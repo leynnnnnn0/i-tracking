91c1986da351abe1ee7144bbe9b3ee94de7d8d05
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-13 10:41:39 +0800

    added actions button on delete history index page

--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -53,7 +53,7 @@
 
         </section>
         <section class="flex justify-end gap-3">
-            <a href="/equipments" class="px-4 py-1 border border-gray-500 rounded-lg text-black hover:bg-opacity-75 transition-colors duration-300">Cancel</a>
+            <a href="/users" class="px-4 py-1 border border-gray-500 rounded-lg text-black hover:bg-opacity-75 transition-colors duration-300">Cancel</a>
             <x-primary-button wire:click="submit">Submit</x-primary-button>
         </section>
     </div>
