36b490c8ffd286d445e0a20c8b341cf3ed6e3c46
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-16 20:29:21 +0800

    updated filter function for supplies history

--- app/Livewire/SupplyHistory.php
+++ app/Livewire/SupplyHistory.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\SupplyHistory as ModelsSupplyHistory;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Symfony\Contracts\Service\Attribute\Required;
@@ -10,23 +11,30 @@ use Symfony\Contracts\Service\Attribute\Required;
 class SupplyHistory extends Component
 {
     use WithPagination;
-    #[Required]
     public $from;
-    #[Required]
     public $to;
 
+    #[On('filter-supplies-history')]
     public function render()
     {
-        if (!$this->from || !$this->to) {
-            $history = ModelsSupplyHistory::with('supply')->latest()->paginate(10);
-        } else {
-            $history = ModelsSupplyHistory::with('supply')
-                ->whereBetween('created_at', [$this->from, $this->to])
-                ->latest()
-                ->paginate(10);
+        $query = ModelsSupplyHistory::query()->with('supply');
+        if ($this->from && $this->to) {
+            $query->whereBetween('created_at', [$this->from, $this->to]);
         }
+        $history = $query->latest()->paginate(10);
         return view('livewire.supply-history', [
             'history' => $history
         ]);
+    }
+
+    public function resetFilter()
+    {
+        $this->from = null;
+        $this->to = null;
+    }
+
+    public function filter()
+    {
+        $this->dispatch('filter-supplies-history');
     }
 }
--- resources/views/livewire/supply-history.blade.php
+++ resources/views/livewire/supply-history.blade.php
@@ -1,10 +1,16 @@
 <div>
     <div class="space-y-3">
         <x-plain-heading>Supplies History</x-plain-heading>
-        <div class="flex items-centers gap-3">
-            <x-form.input label="From" name="from" type="date" wire:model.live="from" />
-            <x-form.input label="To" name="to" type="date" wire:model.live="to" />
-            <x-primary-button class="h-fit self-center">Search</x-primary-button>
+        <!-- Filter -->
+        <div class="bg-white rounded-lg h-fit p-3 flex items-center gap-3 justify-between">
+            <div class="flex items-end gap-3">
+                <x-form.input label="From" name="from" type="date" wire:model="from" />
+                <x-form.input label="To" name="to" type="date" wire:model="to" />
+            </div>
+            <div class="flex items-center gap-3">
+                <button wire:click="resetFilter" class="hover:bg-green-100 transition-colors duration-300 border border-gray-300 px-3 py-1 rounded-lg text-gray-500">Reset filter</button>
+                <x-primary-button class="h-fit self-center" wire:click="filter">Filter</x-primary-button>
+            </div>
         </div>
         <x-table>
             <x-tr>
