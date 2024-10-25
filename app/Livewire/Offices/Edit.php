2a9f3296c9029bdd2d7a8b2345f8aebbe1fa1468
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-25 20:50:13 +0800

    create an updatable trait

--- app/Livewire/Offices/Edit.php
+++ app/Livewire/Offices/Edit.php
@@ -2,44 +2,36 @@
 
 namespace App\Livewire\Offices;
 
-use App\Livewire\Forms\ActivityLogForm;
 use App\Livewire\Forms\OfficeForm;
 use App\Models\Office;
-use Exception;
-use Illuminate\Support\Facades\DB;
+use App\Traits\Updatable;
 use Livewire\Component;
-use Masmerise\Toaster\Toaster;
+
 
 class Edit extends Component
 {
+    use Updatable;
     public $office;
     public OfficeForm $form;
-    public ActivityLogForm $activityLogForm;
+
+    protected function getRedirectRoute(): string
+    {
+        return 'offices';
+    }
+
+    protected function performStoreAction()
+    {
+        return $this->form->update($this->office);
+    }
 
     public function mount($id)
     {
         $this->office = Office::findOrFail($id);
         $this->form->setOfficeForm($this->office);
     }
+
     public function render()
     {
         return view('livewire.offices.edit');
-    }
-
-    public function update()
-    {
-        $this->dispatch('Confirm Update');
-        try {
-            DB::transaction(function () {
-                $office = $this->form->update($this->office);
-                $this->activityLogForm->setActivityLog($this->office, $office, 'Updated Office', 'Update');
-                $this->activityLogForm->store();
-            });
-            Toaster::success('Updated Successfully');
-            return $this->redirect('/offices', true);
-        } catch (Exception $e) {
-            Toaster::error($e->getMessage());
-            throw $e;
-        }
     }
 }
--- app/Traits/Updatable.php
+++ app/Traits/Updatable.php
@@ -4,12 +4,10 @@ namespace App\Traits;
 
 use Exception;
 use Illuminate\Support\Facades\DB;
-use Livewire\Form;
 use Masmerise\Toaster\Toaster;
 
 trait Updatable
 {
-    abstract protected function getModelName(): string;
     abstract protected function getRedirectRoute(): string;
     abstract protected function performStoreAction();
 
@@ -18,13 +16,9 @@ trait Updatable
         $this->dispatch('Confirm Update');
         $this->form->validate();
         try {
-            DB::transaction(function () {
-                $model = $this->performStoreAction();
-                $this->activityLogForm->setActivityLog($model, $model, 'Updated' . $this->getModelName(), 'Update');
-                $this->activityLogForm->store();
-            });
+            $this->performStoreAction();
             Toaster::success('Updated Successfully');
-            return $this->redirect($this->getRedirectRoute(), true);
+            return $this->redirect(route($this->getRedirectRoute() . '.index'), true);
         } catch (Exception $e) {
             Toaster::error($e->getMessage());
         }
