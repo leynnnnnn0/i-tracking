88511b7320805237e674eb0d02664bb23f782e25
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-28 08:43:24 +0800

    added article for new table columns

--- resources/views/livewire/activity-log/view.blade.php
+++ resources/views/livewire/activity-log/view.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.view heading="Activity Details" backLocation="/activity-logs">
+<x-layouts.view heading="Activity" backLocation="/activity-logs">
 
     <div class="col-span-2">
         <p>On {{ $audit->created_at->format('F d, Y H:i:s a')}}, {{ $audit->user->full_name}} {{ $audit->event }} this {{Str::headline( Str::snake(Str::after($audit->auditable_type,'Models\\'), ' ')) }} ID#{{ $audit->auditable_id}}</p>
